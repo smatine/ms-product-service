c74e172447987c44d8ec28fbcd4f38ebb93fe31a
smatine <soufiane.matine@gmail.com>
2017-11-15 12:25:40 +0100

    Update ProductServiceController.java

--- src/main/java/com/sg/microservices/product/rest/ProductServiceController.java
+++ src/main/java/com/sg/microservices/product/rest/ProductServiceController.java
@@ -2,12 +2,10 @@ package com.sg.microservices.product.rest;
 
 import java.util.List;
 
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
-import com.netflix.ribbon.proxy.annotation.Hystrix;
 import com.sg.microservices.product.entity.Product;
 import com.sg.microservices.product.service.IProductService;
 
@@ -29,11 +26,17 @@ public class ProductServiceController {
 
 	private static final Logger logger = LoggerFactory
 			.getLogger(ProductServiceController.class);
+	
+	
+	private static final Logger eslogger = LoggerFactory
+			.getLogger("es-logger");
+	
 	@Autowired
 	private IProductService productService;
 	
+	@Autowired
+	Tracer tracer;
 	
-
 	
 	@HystrixCommand(fallbackMethod = "getDefaultProduct", commandKey = "getAllItems", groupKey = "template-product-service", commandProperties = {
 			@HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "60000"),
@@ -61,6 +64,13 @@ public class ProductServiceController {
 	@RequestMapping(value = "/test", method = RequestMethod.GET, produces = "application/json")
 	@CrossOrigin
 	public String testService() {
+		
+		eslogger.info("Inside Product Service Test Method");
+		
+		eslogger.info("Correlation_id is "  +  tracer.getCurrentSpan().traceIdString());
+		
+		logger.info("Inside Test Method");
+		
 		return "ProductService Sucess from port 8084";
 		//return "ProductService Sucess from port 8085";
 	}
